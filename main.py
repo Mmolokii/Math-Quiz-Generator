0395b2de747e810f09a298aea4e03ce462e67880
Mmoloki Kgololosego <mmolokikgololosego@Mmolokis-MacBook-Air.local>
2025-08-11 14:10:09 +0200

    feat(quiz): implement main quiz loop with scoring and feedback

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ def generate_linear():
     c = a * x + b
     question = f"Solve for x: {a}x + {b} = {c}"
     return question, x
+
+def quiz(num_questions):
+    score = 0
+    for i in range(num_questions):
+        question, answer = generate_linear()
+        print(f"Question {i+1}: {question}")
+        try:
+            user_answer = float(input("Your answer: "))
+            if user_answer == answer:
+                print("Correct!\n")
+                score += 1
+            else:
+                print(f"Incorrect. Corrct answer was {answer}")
+        except ValueError:
+            print("Invalid input. Skipping question.\n")
+    print(f"Your score {score}/{num_questions} {(score/num_questions) * 100:.2f}%")
+
+quiz(5)
